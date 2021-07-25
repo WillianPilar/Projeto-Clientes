d2da583af8a3ea2879b51aab1152ee6576eabf21
willianpilar <willianpilar@hotmail.com>
2021-07-25 17:23:12 -0300

    Incluindo alert

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,6 +10,8 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente : Cliente;
+  success : boolean = false;
+  errors: String[] = [];
 
   constructor( private service : ClientesService) { 
     this.cliente = new Cliente();
@@ -23,6 +25,9 @@ export class ClientesFormComponent implements OnInit {
     this.service.salvar(this.cliente)
       .subscribe( response =>{
         console.log(response);
+        this.success = true;
+      }, errorResponse => {
+        this.errors = errorResponse.error.errors;   
       })
   }
 
